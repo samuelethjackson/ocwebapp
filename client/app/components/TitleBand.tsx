1594c6a68fea71ab5a30c6e764b1615ef92942aa
samuelethjackson <sam@kanon.art>
2024-01-28 14:05:17 +0100

    add navbar animations & about text

--- client/app/components/TitleBand.tsx
+++ client/app/components/TitleBand.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 import CloseIcon from './icons/closeIcon';
 import InvertIcon from './icons/InvertIcon';
@@ -11,19 +11,42 @@ interface TitleBandProps {
 }
 
 const TitleBand: React.FC<TitleBandProps> = ({ pageName }) => {
+  const [isScrolledDown, setIsScrolledDown] = useState(false);
+  const [isScrolledUp, setIsScrolledUp] = useState(false);
   const pathname = usePathname()
+  const pageTitle = "Oceanic Refractions"
   const isAboutPage = pathname === '/about';
 
+  const checkScroll = () => {
+    if (window.scrollY > 0 && !isScrolledDown) {
+      setIsScrolledDown(true);
+      setIsScrolledUp(false);
+    } else if (window.scrollY === 0 && !isScrolledUp) {
+      setIsScrolledUp(true);
+      setIsScrolledDown(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkScroll);
+    return () => {
+      window.removeEventListener('scroll', checkScroll);
+    };
+  }, [isScrolledDown, isScrolledUp]);
+
   return (
-    <div className="absolute top-8 flex flex-row w-screen p-5 justify-between">
+    <div className="fixed top-8 flex flex-row w-screen px-5 py-0 justify-between z-50">
       <div className='w-1/3 flex flex-row gap-0'>
-        <div className="w-full text-base font-normal leading-tight">{pageName}</div>
-        <div className="w-full text-base font-normal leading-tight">Oceanic Refractions</div>
+        <div className='w-full'>
+          <div className={`absolute text-base font-normal leading-tight transition-opacity ${isScrolledDown ? 'duration-1000 ease-in-out opacity-0' : isScrolledUp ? 'delay-1000 duration-1000 ease-in-out opacity-100' : 'opacity-0'}`}>{pageName}</div>
+          <div className={`absolute text-base font-normal leading-tight transition-opacity ${isScrolledDown ? 'delay-1000 duration-1000 ease-in-out opacity-100' : isScrolledUp ? 'duration-1000 ease-in-out opacity-0' : 'opacity-0'}`}>{pageTitle}</div>
+        </div>
+        <div className={`w-full text-base font-normal leading-tight transition-opacity ${isScrolledDown ? 'duration-1000 ease-in-out opacity-0' : isScrolledUp ? 'delay-1000 duration-1000 ease-in-out opacity-100' : 'opacity-0'}`}>{pageTitle}</div>
       </div>
       <div className='w-1/6 flex flex-col md:flex-row justify-between items-center gap-4'>
         {isAboutPage ? <div className='opacity-0'>
           <CloseIcon/>
-        </div> : <div className="w-full text-base font-normal leading-tight">About</div>}
+        </div> : <a href="/about" className="w-full text-base font-normal leading-tight">About</a>}
         <div className='flex flex-col md:flex-row gap-3 center md:opacity-100 opacity-50 hover:opacity-100'>
           <InvertIcon/>
           <ResolutionIcon highRes={true}/>
---
+++ client/app/components/TopBand.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+interface TopBandProps {
+  pageName: string;
+}
+
+const TopBand: React.FC<TopBandProps> = ({ pageName }) => {
+  const [isScrolledDown, setIsScrolledDown] = useState(false);
+  const [isScrolledUp, setIsScrolledUp] = useState(false);
+
+  const checkScroll = () => {
+    if (window.scrollY > 0 && !isScrolledDown) {
+      setIsScrolledDown(true);
+      setIsScrolledUp(false);
+    } else if (window.scrollY === 0 && !isScrolledUp) {
+      setIsScrolledUp(true);
+      setIsScrolledDown(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkScroll);
+    return () => {
+      window.removeEventListener('scroll', checkScroll);
+    };
+  }, [isScrolledDown, isScrolledUp]);
+
+  return (
+    <div className={`fixed top-0 flex flex-row w-screen px-5 py-2 justify-between z-50 transition-opacity ${isScrolledDown ? 'delay-1000 duration-1000 ease-in-out opacity-100 visibility-visible' : isScrolledUp ? 'duration-1000 ease-in-out opacity-0 visibility-hidden' : 'opacity-0 visibility-hidden'}`}>
+      <div className='w-1/3 flex flex-row gap-0'>
+        <div className="w-full text-base font-normal leading-tight">{pageName}</div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBand;
