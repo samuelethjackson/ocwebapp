3a7138f39edacaebb3c636b96165d9bfec8040d0
samuelethjackson <sam@kanon.art>
2024-03-06 17:41:16 +0100

    mobile fixes + caption position desktop

--- client/app/components/ImageElement.tsx
+++ client/app/components/ImageElement.tsx
@@ -10,14 +10,14 @@ const ImageElement: React.FC<ImageElementProps> = ({ image, citation }) => {
   return (
     <div className='md:-ml-[12vw] -ml-5 w-screen gridParent !p-0'>
       <div className="col-start-1 col-end-7 md:col-start-1 md:col-end-12 lg:col-start-1 lg:col-end-25 gridParent !p-0 gap-4 md:gap-0">
-        <div className="col-start-1 lg:col-end-18 row-start-1">
+        <div className="col-start-1 col-end-7 lg:col-end-18 row-start-1">
           <img
             src={image}
-            className='object-contain object-left-bottom w-full max-h-[80vh] !m-0'
+            className='object-contain object-left-bottom w-full max-h-[100vh] !m-0'
           />
         </div>
-        <div className="col-start-1 col-end-7 md:col-start-19 lg:col-end-24 row-start-2 md:row-start-1 h-full w-full flex items-end citation p-3">
-          <p className="citation">
+        <div className="col-start-1 col-end-7 md:col-start-18 lg:col-end-24 row-start-2 md:row-start-1 h-full w-full flex items-end citation pt-0 pb-10 px-3 md:p-0">
+          <p className="pl-0 md:pl-5">
             {citation}
           </p>
         </div>
--- client/app/components/VimeoElement.tsx
+++ client/app/components/VimeoElement.tsx
@@ -18,8 +18,8 @@ const VimeoElement: React.FC<VimeoElementProps> = ({ src, citation }) => {
           height={"100%"}
         />
       </div>
-      <div className="col-start-1 col-end-7 md:col-start-19 lg:col-end-24 row-start-2 md:row-start-1 h-full w-full flex items-end citation p-3">
-        <p className="">
+      <div className="col-start-1 col-end-7 md:col-start-18 lg:col-end-24 row-start-2 md:row-start-1 h-full w-full flex items-end citation pt-0 pb-10 px-3 md:p-0">
+      <p className="pl-0 md:pl-5">
           {citation}
         </p>
       </div>
