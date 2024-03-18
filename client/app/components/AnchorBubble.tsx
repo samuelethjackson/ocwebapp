c8f5d6de266ae53069a66961241853e2ce96863c
samuelethjackson <90724420+samuelethjackson@users.noreply.github.com>
2024-03-18 10:22:57 +0100

    add about scrolling mobile

---
+++ client/app/components/AnchorBubble.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface AnchorBubbleProps {
+  section: string;
+  title: string;
+  handleScroll: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, id: string) => void;
+  activeSection: string;
+}
+
+const AnchorBubble: React.FC<AnchorBubbleProps> = ({ section, title, handleScroll, activeSection }) => {
+  return (
+    <a
+      href={`#${section}`}
+      onClick={(e) => handleScroll(e, section)}
+      className={`anchor-bubble grow w-full ${activeSection === section ? "opacity-100" : "opacity-50"}`}
+      data-id={section} // Add the data-id attribute here
+    >
+      {title}
+    </a>
+  );
+};
+
+export default AnchorBubble;
